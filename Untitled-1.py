e44cbd440f5efd7b62adab20eb8049e2aefe79dc
ohjinho <ohjinho7@gmail.com>
2021-07-05 20:52:36 +0900

    백준 15904

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,17 @@
-print("hi")
+str = input()
+
+words = ["U","C","P","C"]
+
+check = 0
+
+for i in words:
+    if i in str:
+        check+=1
+        str = str[str.find(i)+1:]
+        continue
+    else:
+        print("I hate UCPC")
+        break
+
+if check == 4:
+    print("I love UCPC")
