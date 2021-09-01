02d26bd30751df639185b414a651f85b44e2fda7
ohjinho <ohjinho7@gmail.com>
2021-09-01 22:49:04 +0900

    동빈나 1로만들기

---
+++ Python/다이나믹 프로그래밍/동빈나 1로 만들기.py
@@ -0,0 +1,39 @@
+X = int(input())
+
+d = [0]*30001
+
+for i in range(2, X+1):
+    d[i] = d[i-1] +1
+
+    if i % 2 ==0:
+        d[i] =min(d[i],d[i//2]+1)
+
+    if i%3 ==0:
+        d[i] = min(d[i],d[i//3]+1)
+    if i%5 ==0:
+        d[i] = min(d[i],d[i//5]+1)
+
+print(d[X])
+
+
+
+# 내 풀이
+
+# N = int(input())
+
+# count =0
+
+# while N != 1:
+#     if N % 5==0:
+#         N = N/5
+#         count +=1
+#     elif N<5:
+#         if N%2 == 0:
+#             N = N//2
+#         elif N%3 == 0:
+#             N = N//3
+#         count +=1
+#     else:
+#         N = N-1
+#         count +=1
+# print(count)
