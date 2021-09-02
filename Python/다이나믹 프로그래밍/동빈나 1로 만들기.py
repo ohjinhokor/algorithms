22307ee3f83f312e7b0107d5b36a7a47ebabe929
ohjinho <ohjinho7@gmail.com>
2021-09-02 23:49:54 +0900

    동적프로그래밍 피보나치, 동빈나 개미전사

--- Python/다이나믹 프로그래밍/동빈나 1로 만들기.py
+++ Python/다이나믹 프로그래밍/동빈나 1로 만들기.py
@@ -5,9 +5,8 @@ d = [0]*30001
 for i in range(2, X+1):
     d[i] = d[i-1] +1
 
-    if i % 2 ==0:
-        d[i] =min(d[i],d[i//2]+1)
-
+    if i%2 ==0:
+        d[i] = min(d[i],d[i//2]+1)
     if i%3 ==0:
         d[i] = min(d[i],d[i//3]+1)
     if i%5 ==0:
@@ -37,3 +36,6 @@ print(d[X])
 #         N = N-1
 #         count +=1
 # print(count)
+
+
+
---
+++ Python/다이나믹 프로그래밍/피보나치 함수 소스코드.py
@@ -0,0 +1,39 @@
+def fibo(x):
+    if x==1 or x==2:
+        return 1
+    return fibo(x-1) + fibo(x-2)
+
+print(fibo(4))
+
+# 위처럼 피보나치 수열을 재귀함수로 구현하는건 비효율적
+
+
+d = [0]*100
+
+def fibo2(x):
+    if x == 1 or x == 2:
+        return 1
+    
+    if d[x]!=0:
+        return d[x]
+
+    d[x] = fibo2(x-1) + fibo2(x-2)
+    return d[x]
+
+print(fibo2(99))
+
+# 탑다운 방식, 메모이제이션기법임. 시간복잡도 : O(N)
+
+d2 =[0]*100
+
+d2[1]=1
+d2[2]=1
+n=99
+
+for i in range(3, n+1):
+    d2[i] = d[i-1] + d[i-2]
+    
+print(d2[6])
+
+#바텀업 방식(상향식), 재귀 함수의 스택크기가 한정되어 있을 수 있기 때문에 바텀업 방식이 더 효율적이다.
+# 
