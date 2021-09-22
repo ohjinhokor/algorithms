5e948436154c8e11649ec4746cdde08121379596
ohjinho <ohjinho7@gmail.com>
2021-09-22 22:04:01 +0900

    프로그래머스 체육복(그리디)

---
+++ Java/algorithms/greedy/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b[] = {3};
+        int c[]= {1};
+
+        System.out.println(programmers_체육복.solution(a,b,c));
+
+    }
+}
