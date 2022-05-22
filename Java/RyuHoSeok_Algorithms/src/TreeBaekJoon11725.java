3623eea236001dae6180a5c5fe77b28e3ff27d57
ohjinhokor <ohjinho7@gmail.com>
2022-05-22 20:17:18 +0900

    트리 (백준 11725) - dfs로 다시 풀기

--- Java/RyuHoSeok_Algorithms/src/TreeBaekJoon11725.java
+++ Java/RyuHoSeok_Algorithms/src/TreeBaekJoon11725.java
@@ -1,57 +1,60 @@
 import java.io.*;
 import java.util.*;
 
-public class BaekJoon11725 {
+public class TreeBaekJoon11725 {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
     static int N;
-    static int[] parent;
-    static ArrayList<Integer>[] adj;
+    static int[] parentNode;
     static boolean[] visit;
+
+    static ArrayList<Integer>[] connectedNodes;
 
     static void input() {
         N = scan.nextInt();
-        adj = new ArrayList[N + 1];
-        parent = new int[N + 1];
-        for (int i = 1;i <= N; i++)
-            adj[i] = new ArrayList<>();
-        for (int i = 0; i < N - 1; i++) {
-            int x = scan.nextInt(), y = scan.nextInt();
-            adj[x].add(y);
-            adj[y].add(x);
+
+        parentNode = new int[N + 1];
+        visit = new boolean[N + 1];
+        connectedNodes = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            connectedNodes[i] = new ArrayList<>();
+        }
+        for (int i = 1; i < N; i++) {
+            int firstNode = scan.nextInt();
+            int secondNode = scan.nextInt();
+
+            connectedNodes[firstNode].add(secondNode);
+            connectedNodes[secondNode].add(firstNode);
         }
     }
 
     // start 에서 시작해서 갈 수 있는 정점들을 모두 탐색하기
-    static void bfs(int start) {
-        Queue<Integer> que = new LinkedList<>();
+    static void dfs(int start) {
+        List<Integer> nodeList = new ArrayList<Integer>();
 
-        // start는 방문 가능한 점이므로 que에 넣어준다.
-        que.add(start);
-        visit[start] = true;  // start를 갈 수 있다고 표시하기 (중요!!!)
-
-        while (!que.isEmpty()) {  // 더 확인할 점이 없다면 정지
-            int x = que.poll();
-
-            for (int y: adj[x]){
-                if (visit[y]) continue;  // x 에서 y 를 갈 수는 있지만, 이미 탐색한 점이면 무시
-
-                // y를 갈 수 있으니까 que에 추가하고, visit 처리 하기!
-                que.add(y);
-                parent[y] = x;
-                visit[y] = true;
+        for (Integer node : connectedNodes[start]) {
+            if (visit[node]) {
+                continue;
             }
+            nodeList.add(node);
+            visit[node] = true;
+            parentNode[node] = start;
+        }
+        while (!nodeList.isEmpty()) {
+            Integer node = nodeList.remove(0);
+            dfs(node);
         }
     }
 
     static void pro() {
-        visit = new boolean[N + 1];
-        bfs(1);
-        int ans = 0;
-        for (int i = 2; i <= N; i++)
-            sb.append(parent[i]).append('\n');
-        System.out.println(sb);
+        dfs(1);
+        for (int i = 2; i <= N; i++) {
+            sb.append(parentNode[i] + "\n");
+        }
+
+        System.out.println(sb.toString());
     }
 
     public static void main(String[] args) {
