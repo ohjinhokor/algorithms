dc5a3a3ea093f7fe2efeb6636969b11cfefb0dfe
ohjinhokor <ohjinho7@gmail.com>
2022-07-21 20:05:58 +0900

    Two Pointers (기본 코드)

--- Java/RyuHoSeok_Algorithms/src/basic/BasicTwoPointers.java
+++ Java/RyuHoSeok_Algorithms/src/basic/BasicTwoPointers.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 10,000 이하의 자연수로 이루어진 길이 N짜리 수열이 주어진다. 이 수열에서 연속된 수들의 부분합 중에 그 합이 S 이상이 되는 것 중, 가장 짧은 것의 길이를 구하는 프로그램을 작성하시오.
 public class BasicTwoPointers {
 
 	private static FastReader reader = new FastReader();
