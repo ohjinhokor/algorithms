89f0b67d06792a99c7fc83ed0162fe23fcfb2acb
ohjinhokor <ohjinho7@gmail.com>
2022-07-21 20:04:18 +0900

    Two Pointers (기본 코드)

---
+++ Java/RyuHoSeok_Algorithms/src/basic/BasicTwoPointers.java
@@ -0,0 +1,102 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BasicTwoPointers {
+
+	private static FastReader reader = new FastReader();
+	private static int N;
+	private static int S;
+	private static int[] list;
+
+	public static void main(String[] args) {
+		input();
+		System.out.println(getShortestLength());
+	}
+
+	private static int getShortestLength() {
+		int shortestLength = N + 1;
+
+		int leftPoint = 0;
+		int rightPoint = 0;
+		int sum = 0;
+
+		while (leftPoint < N) {
+			leftPoint++;
+			sum = sum - list[leftPoint - 1];
+			while (sum < S && rightPoint + 1 <= N) {
+				rightPoint = rightPoint + 1;
+				sum = sum + list[rightPoint];
+			}
+			if (sum >= S) {
+				shortestLength = Math.min(shortestLength, (rightPoint - leftPoint + 1));
+			}
+		}
+		if (shortestLength == N + 1) {
+			shortestLength = 0;
+		}
+		return shortestLength;
+	}
+
+	static private void input() {
+		N = reader.nextInt();
+		S = reader.nextInt();
+		list = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			list[i] = reader.nextInt();
+		}
+	}
+
+	static class FastReader {
+
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
